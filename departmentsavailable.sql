4968a89fbac5ce0815c042c8e94235f516af2e11
P Radha Krishna <pradha.krishna.cse17@itbhu.ac.in>
2019-10-28 18:03:00 +0530

    Added Queries
    Added queries for subcompany and departments available tables.

---
+++ departmentsavailable.sql
@@ -0,0 +1,5 @@
+INSERT INTO DepartmentsAvailable(Company_ID,Departments) VALUES(?,?)
+SELECT * FROM DepartmentsAvailable WHERE Company_ID=? AND Departments=?
+SELECT COUNT(*) FROM DepartmentsAvailable WHERE Company_ID=? AND Departments=?
+SELECT * FROM DepartmentsAvailable WHERE Company_ID=?
+SELECT COUNT(*) FROM DepartmentsAvailable WHERE Company_ID=?
