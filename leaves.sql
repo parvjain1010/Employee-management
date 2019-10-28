0c1edbdf97af4a123db4d24fd721a1259defec62
P Radha Krishna <pradha.krishna.cse17@itbhu.ac.in>
2019-10-28 21:50:04 +0530

    Added leaves and regions queries
    Added queries related to leaves table.
    Added queries related to regions table.

---
+++ leaves.sql
@@ -0,0 +1,6 @@
+SELECT * FROM Leaves WHERE Leave_ID=?
+SELECT * FROM Leaves WHERE Name=? and MinDuration=? and MaxDuration=?
+SELECT COUNT(*) FROM Leaves WHERE Leave_ID=?
+SELECT COUNT(*) FROM Leaves WHERE Name=? and MinDuration=? and MaxDuration=?
+DELETE FROM Leaves WHERE Leave_ID=?
+UPDATE Leaves SET Name =?,MaxDuration=?,MinDuration=? WHERE Leave_ID=?
