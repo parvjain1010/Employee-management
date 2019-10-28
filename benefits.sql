0d69ee41717df02161cab0783755b924cfc85e63
P Radha Krishna <pradha.krishna.cse17@itbhu.ac.in>
2019-10-28 21:01:18 +0530

    Added benefits
    added queries for benefits table.

---
+++ benefits.sql
@@ -0,0 +1,4 @@
+UPDATE Benefits SET Type =? AND Name=? WHERE Benefit_ID=?
+SELECT * FROM Benefits WHERE Benefit_ID=?
+SELECT COUNT(*) FROM Benefits WHERE Type=? AND Name=?
+SELECT * FROM Benefits WHERE Type=? AND Name=?
