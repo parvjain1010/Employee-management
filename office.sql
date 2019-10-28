1811b76f49a7d1eb245f09427f79388291fea726
P Radha Krishna <pradha.krishna.cse17@itbhu.ac.in>
2019-10-28 21:40:45 +0530

    added personal relations and office queries
    Added queries related to personal relations table.
    Added queries related to office table.

---
+++ office.sql
@@ -0,0 +1,7 @@
+UPDATE Office SET Mailing_Address=? AND Company=? AND LocatedAt=? WHERE Office_ID=?
+UPDATE Office SET Company=? WHERE Office_ID=?
+SELECT * FROM Office WHERE Office_ID=?
+SELECT * FROM Office WHERE Mailing_Address=? and Company=? and LocatedAt=?
+SELECT * FROM Office
+SELECT COUNT(*) FROM Office WHERE Office_ID=?
+SELECT COUNT(*) FROM Office WHERE Mailing_Address=? and Company=? and LocatedAt=?
